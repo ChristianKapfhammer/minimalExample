1b179cf7081d566d84ded28e10e7e7ab869539fc
Christian Kapfhammer <christian_kapfhammer@gmx.de>
2017-02-24 11:38:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./Hercules\(original\)/ifdeftoif.sh --bdd --performance -I /usr/include/x86_64-linux-gnu -I /usr/include --smallFeatureModelDimacs ./model.dimacs --featureModelFExpr ./model.fm ./ast_test.c
+./Hercules\(original\)/ifdeftoif.sh --bdd --performance --platfromHeader ./platform.h -I /usr/include/x86_64-linux-gnu -I /usr/include --smallFeatureModelDimacs ./model.dimacs --featureModelFExpr ./model.fm ./ast_test.c
